9ca138dc9bc40fbb6df99e0f8ea5c585e75b09be
kkwasnik <kfkwasnik@gmail.com>
2026-02-27 20:00:49 -0500

    core grocery agent setup

---
+++ agent.py
@@ -0,0 +1,36 @@
+import os
+from dotenv import load_dotenv
+from strands import Agent
+from strands.models.ollama import OllamaModel
+
+load_dotenv()
+
+model = OllamaModel(
+    model_id="gpt-oss:120b-cloud",
+    host="https://ollama.com"
+)
+
+agent = Agent(
+    model=model,
+   
+    system_prompt="You are an grocery shopping agent. Please prioritize the users preferences."
+)
+
+
+if __name__ == "__main__":
+    print("Welcome to your grocery assistant! Type 'exit' to quit.")
+    
+    while True:
+        user_input = input("\nYou: ")
+        
+        # Check for exit commands
+        if user_input.lower() in ['exit', 'quit']:
+            print("Exiting Grocery Assitant. Goodbye!")
+            break
+            
+        # The agent maintains conversation history automatically 
+        try:
+            response = agent(user_input)
+            print(f"\nAgent: {response}")
+        except Exception as e:
+            print(f"\nAgent Error: Whoops, something went wrong. ({e})")
--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from weekly-food-shopping!")
-
-
-if __name__ == "__main__":
-    main()
